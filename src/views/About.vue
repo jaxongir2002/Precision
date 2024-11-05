527cfef9f2b9f26502bc2b8ec6f61bedde5253ef
user <jaxongirxon.rustamov12@gmail.com>
2024-11-05 11:38:51 +0500

    bug fix

--- src/views/About.vue
+++ src/views/About.vue
@@ -87,15 +87,7 @@ onBeforeUnmount(() => {
   <Collaboration/>
   <Footer/>
 </template>
-<style>
 
-//::-webkit-scrollbar-thumb {
-//  background: #F5733D ;
-//}
-//::-webkit-scrollbar-track {
-//  background: #F2EDE8;
-//}
-</style>
 <style scoped lang="scss">
 .hero {
   width: 100%;
